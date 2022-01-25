2d29e36c75c4e596c46955a594d3ab80680d3bcb
Danish Anwar <da.ruvyx@gmail.com>
2022-01-25 20:58:29 +0500

    file will be checked in both dynamic and pre-compiled assets

--- lib/premailer/rails/css_loaders/asset_pipeline_loader.rb
+++ lib/premailer/rails/css_loaders/asset_pipeline_loader.rb
@@ -8,19 +8,20 @@ class Premailer
           return unless asset_pipeline_present?
 
           file = file_name(url)
-          ::Rails.application.assets_manifest.find_sources(file).first
-        rescue Errno::ENOENT, TypeError => _error
+
+          find_source file
+        rescue Errno::ENOENT, TypeError => _e
         end
 
         def file_name(url)
-          prefix = File.join(
-            ::Rails.configuration.relative_url_root.to_s,
-            ::Rails.configuration.assets.prefix.to_s,
+          prefix = [
+            ::Rails.configuration.relative_url_root,
+            ::Rails.configuration.assets.prefix,
             '/'
-          )
+          ].join
           URI(url).path
-            .sub(/\A#{prefix}/, '')
-            .sub(/-(\h{32}|\h{64})\.css\z/, '.css')
+                  .sub(/\A#{prefix}/, '')
+                  .sub(/-(\h{32}|\h{64})\.css\z/, '.css')
         end
 
         def asset_pipeline_present?
@@ -29,6 +30,16 @@ class Premailer
             ::Rails.application &&
             ::Rails.application.respond_to?(:assets_manifest) &&
             ::Rails.application.assets_manifest
+        end
+
+        def find_source(file_name)
+          if ::Rails.configuration.assets.compile
+            # Dynamic compilation
+            ::Rails.application.assets.find_asset(file_name)
+          else
+            # Pre-compiled
+            ::Rails.application.assets_manifest.assets[file_name]
+          end
         end
       end
     end
